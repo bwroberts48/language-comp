c2d6e0d8da8d776ece8bff29a70efcc3a093b5ae
brett.roberts <brett.roberts48@gmail.com>
2020-02-22 00:08:04 -0800

    Initial commit with starting structure

---
+++ Compiler/LangTokens/Symbol.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LangTokens
+{
+    public class Symbol
+    {
+        public string Word
+        {
+            get { return this._word; }
+            set { this._word = value; }
+        }
+
+        public SymbolTypeStrategy Type
+        {
+            get { return this._type; }
+            set { this._type = value; }
+        }
+
+        private string _word;
+        private SymbolTypeStrategy _type;
+    }
+}
---
+++ Compiler/LangTokens/SymbolTypeStrategy.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LangTokens
+{
+    abstract public class SymbolTypeStrategy
+    {
+        abstract public void ToDictionaryForm();
+    }
+}

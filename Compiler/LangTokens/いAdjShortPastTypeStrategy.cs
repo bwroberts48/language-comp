9a9e05ef57973aa2e29f40a2ef8ccd2929ad7059
brett.roberts <brett.roberts48@gmail.com>
2020-02-22 00:29:56 -0800

    Removed unneeded intermediate class and made first strategy

---
+++ Compiler/LangTokens/いAdjShortPastTypeStrategy.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LangTokens
+{
+    class いAdjShortPastTypeStrategy : SymbolTypeStrategy
+    {
+        //Converts the type and returns it
+        public override string ToDictionaryForm(string word)
+        {
+            //Takes the "かった" off of the adjective
+            string dictForm = word.Substring(0, word.Length - 3);
+            //Adds the い back, lost after it was conjugated
+            dictForm += "い";
+
+            return dictForm;
+        }
+    }
+}
